668d49eb4b0c45e4b977e267d5db777e4e887961
sabermaraghi <120035678+sabermaraghi@users.noreply.github.com>
2025-11-05 14:40:42 +0330

    Update arguments.sh script

--- scripts/basics/arguments.sh
+++ scripts/basics/arguments.sh
@@ -3,9 +3,9 @@ set -euo pipefail
 
 echo "Script name: $0"
 echo "Number of args: $#"
-echo "First arg: $1"
-echo "Second arg: $2"
-echo "All args: $@"
+echo "First arg: ${1:-}"
+echo "Second arg: ${2:-}"
+echo "All args: $*"
 
 # Loop over args
 for arg in "$@"; do
