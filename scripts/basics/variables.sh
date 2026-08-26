e178aaee5802084e8d2665b6591ce8b69eb7fb4b
ericvalijani <ericvalijani@outlook.com>
2026-08-26 20:35:31 +0330

    NEW

--- scripts/basics/variables.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-name="Alice"
-age=30
-echo "Name: $name, Age: $age"
-
-readonly pi=3.14
-echo "Pi: $pi"
--- scripts/projects/ci-cd-pro/steps/10-lint.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-shopt -s globstar
-source "$(dirname "${BASH_SOURCE[0]}")/../lib/log.sh"
-log "Running shellcheck & shfmt"
-shellcheck --severity=error -x -e SC1091 scripts/**/*.sh
